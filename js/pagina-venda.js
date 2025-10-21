1d8bac51f9268c48ff0b5e796c2889f93269277d
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-10-20 22:08:47 -0300

    Finalização do menu logado (com responsividade no dropdown)

--- js/pagina-venda.js
+++ js/pagina-venda.js
@@ -1,3 +1,4 @@
+//carrosel
 const prevBtn = document.querySelector('.carousel-btn.prev');
 const nextBtn = document.querySelector('.carousel-btn.next');
 const carousel = document.querySelector('.product-carousel');
@@ -12,3 +13,52 @@ prevBtn.addEventListener('click', () => {
   carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
 });
 
+
+//menu itens
+const dropdowns = document.querySelectorAll('.shopping-cart-order-service .dropdown');
+
+dropdowns.forEach(dropdown => {
+  const link = dropdown.querySelector('a');
+
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    dropdowns.forEach(item => {
+      if (item !== dropdown) {
+        item.classList.remove('ativo');
+      }
+    });
+
+    dropdown.classList.toggle('ativo');
+  });
+});
+
+document.addEventListener('click', e => {
+  if (![...dropdowns].some(dropdown => dropdown.contains(e.target))) {
+    dropdowns.forEach(dropdown => dropdown.classList.remove('ativo'));
+  }
+});
+
+
+
+//perfil itens
+const dropdownsPerfil = document.querySelectorAll('.dropdown-perfil');
+
+function toggleDropdown(e) {
+  e.preventDefault();
+
+  if (this.classList.contains('ativo')) {
+    this.classList.remove('ativo');
+  } else {
+    dropdownsPerfil.forEach((item) => item.classList.remove('ativo'));
+    this.classList.add('ativo');
+  }
+}
+
+dropdownsPerfil.forEach((item) => item.addEventListener('click', toggleDropdown));
+
+document.addEventListener('click', (e) => {
+  if (![...dropdownsPerfil].some((item) => item.contains(e.target))) {
+    dropdownsPerfil.forEach((item) => item.classList.remove('ativo'));
+  }
+});
