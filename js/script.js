3f3618c179bd319bfb8177b067d87223fd121c62
jtn-san <santosjonnathan@aol.com>
2025-10-21 21:28:45 -0300

    Aplicação da responsividade e efeitos.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.1 });
 
 elements.forEach(el => observer.observe(el));
+
+
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+// Abrir/fechar menu ao clicar no hamburger
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+// Fecha o menu ao clicar em um link
+document.querySelectorAll(".menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  });
+});
+
+// Fecha o menu ao clicar fora dele
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !hamburger.contains(e.target)) {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  }
+});
+
+
