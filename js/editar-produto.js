a26562a20f38dc59f663f25d7f5b68157077c370
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-10-30 20:54:35 -0300

    finalização da pagina de editar produtos

--- js/editar-produto.js
+++ js/editar-produto.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         if (produto) {
             preencherProduto(produto);
-            form.style.display = "block";
+            // form.style.display = "block";
         }
 
     } finally {
